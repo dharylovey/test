f09b265d83d9b6ef74caa9cae59e04d68b73ba95
Dharyl Carry S. Almora <dharylovey@gmail.com>
2024-11-17 11:05:33 +0800

    🛠️fix project section padding in medium device

--- src/components/section/FeaturedWork.tsx
+++ src/components/section/FeaturedWork.tsx
@@ -1,3 +1,11 @@
 export default function FeaturedWork() {
-  return <div>FeaturedWork</div>;
+  return (
+    <div className="bg-white">
+      <div className="h-full mb-2 py-2 max-w-7xl mx-auto p-2 md:p-0">
+        <div className="flex justify-between py-4">
+          <h1 className="text-center md:text-start text-3xl">Projects</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
